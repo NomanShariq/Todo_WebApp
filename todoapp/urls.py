823acf86ea9d6109fb81ab3ed19912b508926b78
Syed Noman Shariq <nshariq562@gmail.com>
2024-01-30 02:32:30 +0500

    Task can be search , Edit & Delete

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('delete/<str:pk>', views.delete,name='delete'),
+    path('edit/<int:pk>', views.edit,name='edit'),
     path('login/', views.loginPage , name='login'),
     path('register/', views.registerPage , name='register'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render, redirect
+
+from todoapp.forms import TodoForm
 from .models import Todo
 
 # Create your views here.
 def index(request):
-    todo = Todo.objects.all()
+    todos = Todo.objects.all()
+
+    # Handle search
+    search_query = request.GET.get('search', '')
+    if search_query:
+        todos = todos.filter(title__icontains=search_query)
 
     if request.method == 'POST':
-        new_todo = Todo(
-            title = request.POST['title']
-        )
+        new_todo = Todo(title=request.POST['title'])
         new_todo.save()
         return redirect('/')
 
-
-    return render(request, 'index.html', {'todos': todo})
+    return render(request, 'index.html', {'todos': todos, 'search_query': search_query})
 
 def delete(request, pk):
     todo = Todo.objects.get(id=pk)
     todo.delete()
     return redirect('/')
+
+def edit(request, pk):
+    todo = Todo.objects.get(id=pk)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('home')
+    else:
+        form = TodoForm(instance=todo)
+
+    return render(request, 'edit_task.html', {'form': form , 'todo' : todo})
 
 def loginPage(request):
     return render(request, 'login&Signup.html')
